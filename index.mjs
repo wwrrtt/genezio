787f5ccbc4a685c4baa50c9b6025cb66ec0b7a75
Radu Dumitrescu <d.raduandrei@gmail.com>
2024-06-13 14:47:45 +0300

    small changes

--- index.mjs
+++ index.mjs
@@ -7,12 +7,17 @@ app.get("/", (req, res) => {
   res.send("Hello World from Express serverless!");
 });
 
-// You don't need to listen to the port when using serverless
+app.get("/users", (req, res) => {
+  res.json([{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]);
+});
 
-// app.listen(8080, () => {
-//   console.log(
-//     "Server is running on port 8080. Check the app on http://localhost:8080"
-//   );
-// });
+// You don't need to listen to the port when using serverless functions in production
+if (process.env.NODE_ENV === "dev") {
+  app.listen(8080, () => {
+    console.log(
+      "Server is running on port 8080. Check the app on http://localhost:8080"
+    );
+  });
+}
 
 export const handler = Serverless(app);
