8e094567fc53cc97fcb0f804477b53b62948e56e
Cristi Miloiu <miloiuc4@gmail.com>
2024-10-22 11:01:56 +0300

    remove serverless-http

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import Serverless from "serverless-http";
 
 const app = express();
 
@@ -11,13 +10,8 @@ app.get("/users", (req, res) => {
   res.json([{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]);
 });
 
-// You don't need to listen to the port when using serverless functions in production
-if (process.env.NODE_ENV === "dev") {
-  app.listen(8080, () => {
+app.listen(8080, () => {
     console.log(
       "Server is running on port 8080. Check the app on http://localhost:8080"
     );
-  });
-}
-
-export const handler = Serverless(app);
+});
