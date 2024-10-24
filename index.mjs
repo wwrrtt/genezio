5000e846f33f12da7a468233ee0d6cfd35192fde
Radu Dumitrescu <d.raduandrei@gmail.com>
2024-10-24 11:23:41 +0300

    added example with stream response

--- index.mjs
+++ index.mjs
@@ -10,8 +10,23 @@ app.get("/users", (req, res) => {
   res.json([{ id: 1, name: "Alice" }, { id: 2, name: "Bob" }]);
 });
 
+// route example with streaming
+
+app.get("/stream", (req, res) => {
+  // write a long string to the response
+  const interval = setInterval(() => {
+    res.write("Hello, World! ");
+  }, 1000);
+
+  // end the response after 5 seconds
+  setTimeout(() => {
+    clearInterval(interval);
+    res.end();
+  }, 5000);
+});
+
 app.listen(8080, () => {
-    console.log(
-      "Server is running on port 8080. Check the app on http://localhost:8080"
-    );
+  console.log(
+    "Server is running on port 8080. Check the app on http://localhost:8080"
+  );
 });
