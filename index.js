14df3cc24c0349cc7429c8e915d191eeff71813a
wwrrtt <272449844@qq.com>
2024-11-29 05:28:43 +0000

    ok

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-const net=require('net');
-const {WebSocket,createWebSocketStream}=require('ws');
-const logcb= (...args)=>console.log.bind(this,...args);
-const errcb= (...args)=>console.error.bind(this,...args);
+const http = require('http');
+const fs = require('fs');
+const net = require('net');
+const url = require('url');
+const { Server: WebSocketServer } = require('ws');
+const { createWebSocketStream } = require('ws');
 
 const uuid= (process.env.UUID||'ee1feada-4e2f-4dc3-aaa6-f97aeed0286b').replaceAll('-','');
 const port= process.env.PORT||8080;
 
-const wss=new WebSocket.Server({port},logcb('listen:', port));
-wss.on('connection', ws=>{
-  ws.once('message', msg=>{
-    const [VERSION]=msg;
-    const id=msg.slice(1, 17);
-    if(!id.every((v,i)=>v==parseInt(uuid.substr(i*2,2),16))) return;
-    let i = msg.slice(17, 18).readUInt8()+19;
-    const port = msg.slice(i, i+=2).readUInt16BE(0);
-    const ATYP = msg.slice(i, i+=1).readUInt8();
-    const host= ATYP==1? msg.slice(i,i+=4).join('.')://IPV4
-    (ATYP==2? new TextDecoder().decode(msg.slice(i+1, i+=1+msg.slice(i,i+1).readUInt8()))://domain
-    (ATYP==3? msg.slice(i,i+=16).reduce((s,b,i,a)=>(i%2?s.concat(a.slice(i-1,i+1)):s), []).map(b=>b.readUInt16BE(0).toString(16)).join(':'):''));//ipv6
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
 
-    logcb('conn:', host,port);
+const wss = new WebSocketServer({ noServer: true });
+
+server.on('upgrade', (request, socket, head) => {
+  const pathname = url.parse(request.url).pathname;
+  if (pathname === '/ws') {
+    wss.handleUpgrade(request, socket, head, ws => {
+      wss.emit('connection', ws, request);
+    });
+  } else {
+    socket.destroy();
+  }
+});
+
+wss.on('connection', ws => {
+  ws.once('message', msg => {
+    const [VERSION] = msg;
+    const id = msg.slice(1, 17);
+    if (!id.every((v, i) => v == parseInt(uuid.substr(i * 2, 2), 16))) return;
+    let i = msg.slice(17, 18).readUInt8() + 19;
+    const port = msg.slice(i, i += 2).readUInt16BE(0);
+    const ATYP = msg.slice(i, i += 1).readUInt8();
+    const host = ATYP == 1 ? msg.slice(i, i += 4).join('.') : // IPV4
+      (ATYP == 2 ? new TextDecoder().decode(msg.slice(i + 1, i += 1 + msg.slice(i, i + 1).readUInt8())) : // domain
+        (ATYP == 3 ? msg.slice(i, i += 16).reduce((s, b, i, a) => (i % 2 ? s.concat(a.slice(i - 1, i + 1)) : s), []).map(b => b.readUInt16BE(0).toString(16)).join(':') : '')); // ipv6
+
+    console.log('conn:', host, port);
     ws.send(new Uint8Array([VERSION, 0]));
-    const duplex=createWebSocketStream(ws);
-     net.connect({host,port}, function(){
-        this.write(msg.slice(i));
-        duplex.on('error',errcb('E1:')).pipe(this).on('error',errcb('E2:')).pipe(duplex);
-    }).on('error',errcb('Conn-Err:',{host,port}));
-  }).on('error',errcb('EE:'));
+    const duplex = createWebSocketStream(ws);
+    net.connect({ host, port }, function () {
+      this.write(msg.slice(i));
+      duplex.on('error', console.error.bind(this, 'E1:')).pipe(this).on('error', console.error.bind(this, 'E2:')).pipe(duplex);
+    }).on('error', console.error.bind(this, 'Conn-Err:', { host, port }));
+  }).on('error', console.error.bind(this, 'EE:'));
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
